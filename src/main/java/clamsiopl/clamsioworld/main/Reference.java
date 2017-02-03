c4264d3a76d243256cf5ddf329a94eb340fd382d
clamsioPL <daniel.zdziebko.00@gmail.com>
2017-02-03 21:55:21 +0100

    Some overview
    Two files for branch 0.6.3-beta (Minecraft version - 1.10.2)

---
+++ src/main/java/clamsiopl/clamsioworld/main/Reference.java
@@ -0,0 +1,13 @@
+package clamsiopl.clamsioworld.main;
+
+public class Reference
+{
+	public static final String MODID = "clamsioworld";
+	public static final String NAME = "ClamsioWorld";
+	public static final String VERSION = "0.6.3-beta";
+	public static final String MCVERSION = "1.10.2";
+	public static final String TEXTURE_DOMAIN = "clamsioworld:";
+	
+	public static final String CLIENTPROXY = "clamsiopl.clamsioworld.proxy.ClientProxy";
+	public static final String COMMONPROXY = "clamsiopl.clamsioworld.proxy.CommonProxy";
+}
